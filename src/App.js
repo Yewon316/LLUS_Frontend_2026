01e6fbbdedab7c02fcce2d3a9f4fd80937aeeb35
Aaron Min <85600601+aaronmin8460@users.noreply.github.com>
2026-02-25 18:24:43 -0500

    detialpage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import StudyPage from "./pages/StudyPage";
 import ProjectPage from "./pages/ProjectPage";
 import SportsPage from "./pages/SportsPage";
 import HobbyPage from "./pages/HobbyPage";
+import MeetingDetailPage from "./pages/MeetingDetailPage";
 
 export default function App() {
   return (
@@ -16,6 +17,7 @@ export default function App() {
         <Route path="/project" element={<ProjectPage />} />
         <Route path="/sports" element={<SportsPage />} />
         <Route path="/hobby" element={<HobbyPage />} />
+        <Route path="/meetings/:id" element={<MeetingDetailPage />} />
       </Route>
     </Routes>
   );
